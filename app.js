d3fa26347932e34fa294c7124ad7bfa0ffa769c1
Rajashree Parhi <rajashreeparhi23@gmail.com>
2019-06-28 13:03:46 +0530

    Procfile

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const locationRoutes = require('./api/routes/location');
 
 mongoose.connect('mongodb://localhost:27017/tracking');
 // mongoose.connect(
-//     "mongodb+srv://rajashree:" +process.env.MONGO_ATLAS_PW + "@node-servers-welef.mongodb.net/test?retryWrites=true&w=majority" ,{
-//         useNewUrlParser:true
+//     // "mongodb+srv://rajashree:" +process.env.MONGO_ATLAS_PW + "@node-servers-welef.mongodb.net/test?retryWrites=true&w=majority" 
+//     "mongodb+srv://rajashree:" + process.env.MONGO_ATLAS_PW + "@cluster0-mqswf.mongodb.net/test?retryWrites=true&w=majority"
+//     , {
+//         useNewUrlParser: true
 //     });
-mongoose.Promise=global.Promise;
+mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
 app.use(body.urlencoded({ extended: true }));
